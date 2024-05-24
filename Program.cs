167433baccf98e9e1bda11c8a944f638b40665a6
Matias Grimm <37000378+MatiasGrimm@users.noreply.github.com>
2024-05-24 09:51:50 +0200

    Fixed issue here app couldn't start

--- Program.cs
+++ Program.cs
@@ -3,30 +3,36 @@ using MyGarden_API.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IMailService, MailService>();
+builder.Services.AddScoped<IMailService, MailService>();
+
+// Add environment-specific services
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddScoped<IMailSenderService, DummyMailSenderService>();
+}
+else
+{
+    builder.Services.AddScoped<IMailSenderService, SimpleSmtpMailSenderService>();
+}
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    builder.Services.AddSingleton<IMailSenderService, DummyMailSenderService>();
     app.UseDeveloperExceptionPage();
-    app.UseCors(builder => builder
+    app.UseCors(policy => policy
         .AllowAnyOrigin()
         .AllowAnyMethod()
         .AllowAnyHeader());
 }
 else
 {
-    // production services
-    builder.Services.AddSingleton<IMailSenderService, SimpleSmtpMailSenderService>();
-    app.UseCors(builder => builder
+    app.UseCors(policy => policy
         .WithOrigins("https://tavsogmatias.com")
         .AllowAnyMethod()
         .AllowAnyHeader());

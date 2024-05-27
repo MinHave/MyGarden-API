c3aafbfb786433675c23fc855c7e189b161752d6
Matias Grimm <37000378+MatiasGrimm@users.noreply.github.com>
2024-05-27 10:55:25 +0200

    MG-23-Setup-automapper (#2)

---
+++ ViewModels/SimpleUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MyGarden_API.ViewModels
+{
+    public class SimpleUserViewModel
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public bool IsDisabled { get; set; }
+    }
+}
---
+++ ViewModels/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyGarden_API.ViewModels
+{
+    public class UserViewModel : SimpleUserViewModel
+    {
+        public DateTimeOffset Created { get; set; }
+        
+        public DateTimeOffset Updated { get; set; }
+    }
+}

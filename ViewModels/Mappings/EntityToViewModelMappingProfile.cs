69728a46cbab8fad4e0cd8ceae81455eb6460278
Matias Grimm <37000378+MatiasGrimm@users.noreply.github.com>
2024-05-27 10:50:38 +0200

    Install automapper (setup first viewmodel)

---
+++ ViewModels/Mappings/EntityToViewModelMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MyGarden_API.Models.Entities;
+
+namespace MyGarden_API.ViewModels.Mappings
+{
+    public class EntityToViewModelMappingProfile : Profile
+    {
+        public EntityToViewModelMappingProfile()
+        {
+            CreateMap<ApiUser, UserViewModel>()
+                .ReverseMap();
+        }
+    }
+}

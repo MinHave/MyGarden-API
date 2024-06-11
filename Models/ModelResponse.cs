744093455bc751021977f6b542e2ca57eacc3e23
Matias Grimm <37000378+MatiasGrimm@users.noreply.github.com>
2024-06-11 17:20:12 +0200

    Added ModelResponse

---
+++ Models/ModelResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace MyGarden_API.Models
+{
+    public class ModelResponse<T> : Exception
+    {
+        public int HTTP { get; set; }
+
+        public string CustomMessage { get; set; }
+
+        public string Title { get; set; }
+
+        public T Value { get; set; }
+    }
+}

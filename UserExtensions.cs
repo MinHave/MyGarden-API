c27ea6005401cb55ca607a0a9fc3b4dee8c89220
Matias Grimm <37000378+MatiasGrimm@users.noreply.github.com>
2024-06-11 17:20:48 +0200

    Added missing files: * UserExtensions * NotAuthorizedException

---
+++ UserExtensions.cs
@@ -0,0 +1,27 @@
+﻿using MyGarden_API.API.Helpers;
+using MyGarden_API.Data;
+using MyGarden_API.Models.Entities;
+using System.Security.Claims;
+
+namespace MyGarden_API
+{
+    public static class UserExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal claims)
+        {
+            return claims.FindFirstValue(AuthConstants.JwtClaimIdentifiers.Id);
+        }
+
+        public static ApiUser GetUser(this ClaimsPrincipal claims, ApiDbContext context)
+        {
+            var user = context.Users.Find(claims.GetUserId());
+
+            if (user == null)
+            {
+                throw new InvalidOperationException($"User {claims.GetUserId()} not found");
+            }
+
+            return user;
+        }
+    }
+}

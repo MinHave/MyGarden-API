7cd40293919fcfe30299b122a7ef93a00ccc046a
Matias Grimm <37000378+MatiasGrimm@users.noreply.github.com>
2024-06-11 10:12:53 +0200

    MG-24-Setup-FTP (#3)
    * Add ftpOptions together with FtpService
    
    * Add ftpOptions to appsettings.json

---
+++ Models/FtpOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace MyGarden_API.Models
+{
+    public class FtpOptions
+    {
+        public string FtpAddress { get; set; }
+
+        public string Port { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}

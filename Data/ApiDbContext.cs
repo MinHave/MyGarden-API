4fa64d37d719cbf3b92a402a4411069d91840054
Matias Grimm <37000378+MatiasGrimm@users.noreply.github.com>
2024-06-11 17:09:40 +0200

    Add ApiDbContext

---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyGarden_API.Models;
+using MyGarden_API.Models.Entities;
+
+namespace MyGarden_API.Data
+{
+    public class ApiDbContext : IdentityDbContext<ApiUser>
+    {
+        public ApiDbContext(DbContextOptions options) : base(options)
+        {
+            SavingChanges += ApiDbContext_SavingChanges;
+        }
+
+        public DbSet<AuthRefreshToken> AuthRefreshTokens { get; set; }
+
+        public DbSet<Garden> Gardens { get; set; }
+
+        public DbSet<GardenAccess> GardenAccess { get; set; }
+
+        public DbSet<Plant> Plants { get; set; }
+
+        public DbSet<Image> Images { get; set; }
+
+        private void ApiDbContext_SavingChanges(object sender, SavingChangesEventArgs e)
+        {
+            UpdateCreatedChanged();
+        }
+
+        private void UpdateCreatedChanged()
+        {
+            var now = DateTimeOffset.Now;
+            foreach (var entry in ChangeTracker.Entries<IDatedEntity>())
+            {
+                var entity = entry.Entity;
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entity.Created = now;
+                        entity.Updated = now;
+                        break;
+
+                    case EntityState.Modified:
+                        entity.Updated = now;
+                        break;
+                }
+            }
+
+            ChangeTracker.DetectChanges();
+        }
+    }
+}

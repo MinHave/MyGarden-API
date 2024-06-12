d476e72235d9c5b7475ad2c9b87dfa4c01137f9f
Matias Grimm <37000378+MatiasGrimm@users.noreply.github.com>
2024-06-12 16:24:44 +0200

    Add PlantViewModel

---
+++ ViewModels/PlantViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MyGarden_API.Models.Entities;
+using System.Linq.Expressions;
+
+namespace MyGarden_API.ViewModels
+{
+    public class PlantViewModel
+    {
+        public Guid id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string specie { get; set; }
+    }
+}

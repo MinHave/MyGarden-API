24365a1c38cd47edd54c62dd92349f0ed62daea3
Matias Grimm <37000378+MatiasGrimm@users.noreply.github.com>
2024-06-12 18:26:24 +0200

    Add missing viewmodels

---
+++ ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyGarden_API.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        public string CurrentPassword { get; set; }
+
+        public string NewPassword { get; set; }
+    }
+}
---
+++ ViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MyGarden_API.ViewModels
+{
+    public class ResetPasswordViewModel
+    {
+        public string Username { get; set; }
+
+        public string Token { get; set; }
+
+        public string Password { get; set; }
+    }
+}

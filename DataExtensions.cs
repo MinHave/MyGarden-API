82d9427266aae3759a377deae96ee8b49e3cb2ea
Matias Grimm <37000378+MatiasGrimm@users.noreply.github.com>
2024-06-13 15:19:30 +0200

    Add usefull extension methods

---
+++ DataExtensions.cs
@@ -0,0 +1,20 @@
+﻿using MyGarden_API.Data;
+using MyGarden_API.Models.Entities;
+using MyGarden_API.Models.Entities.Enums;
+using System.Security.Claims;
+
+namespace MyGarden_API
+{
+    public static class DataExtensions
+    {
+        public static IQueryable<Garden> WhereAllowedByUser(this IQueryable<Garden> query, string userId, ApiDbContext context, Access modifier)
+        {
+            var gardenAccess = context.GardenAccess
+                .Where(x => x.UserId == userId && x.Access.HasFlag(modifier))
+                .Select(x => x.Garden.Id)
+                .ToList();
+
+            return query.Where(x => gardenAccess.Contains(x.Id));
+        }
+    }
+}
--- UserExtensions.cs
+++ UserExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using MyGarden_API.API.Helpers;
 using MyGarden_API.Data;
 using MyGarden_API.Models.Entities;
+using MyGarden_API.Models.Entities.Enums;
 using System.Security.Claims;
 
 namespace MyGarden_API
@@ -10,6 +11,10 @@ namespace MyGarden_API
         public static string GetUserId(this ClaimsPrincipal claims)
         {
             return claims.FindFirstValue(AuthConstants.JwtClaimIdentifiers.Id);
+        }
+        public static bool HasPermission(this ClaimsPrincipal claims, ApiDbContext context, Guid gardenId, Access gardenAccess)
+        {
+            return context.GardenAccess.Where(x => x.UserId == claims.GetUserId() && x.Garden.Id == gardenId && x.Access.HasFlag(gardenAccess)).Any();
         }
 
         public static ApiUser GetUser(this ClaimsPrincipal claims, ApiDbContext context)
